933aced89ad3cc3e6a7c9b651bdcdea7c0aeae3b
Yoann Bdl <50846241+YoannBDL@users.noreply.github.com>
2020-10-25 21:10:49 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import UseStateBasics from './tutorial/1-useState/final/2-useState-basics'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <UseStateBasics />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
